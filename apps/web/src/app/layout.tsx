59574330a8d9800e46e63d0411165a5b68131f3c
serge <serge.hall.dev@gmail.com>
2026-09-22 20:21:41 -0700

    fix(web): use Next.js favicon file convention

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -60,11 +60,9 @@ export const metadata: Metadata = {
 
   icons: {
     icon: [
-      { url: '/favicon.ico' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
-    shortcut: ['/favicon.ico'],
     apple: '/apple-touch-icon.png',
   },
 
